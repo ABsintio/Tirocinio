5a87242ce7426234f70378726ccd38a94bf45d3c
ABsintio <absintio098@gmail.com>
2020-10-30 14:43:37 +0100

    Aggiornato file modelica2GPU.py

--- modelica2MPGOSgpuPython/src/modelica2GPU.py
+++ modelica2MPGOSgpuPython/src/modelica2GPU.py
@@ -31,18 +31,47 @@ def createXML(workingdir, modelfilename):
     pass
 
 
-def getdefaultoptions():
+def get_tolerance_fromXML(xmlfile):
+    """ Ritorna la tolleranza """
+    root = ET.parse(xmlfile).getroot()
+    return float(root[1].attrib['tolerance'])
+
+
+def getdefaultoptions(nevents, nstates, xmlfile):
     """ Restituisce una lista di opzioni di default """
     # Come prima cosa dobbiamo prendere la CUDA capability della GPU di default
-    assert cuda.Device.count() > 0, "Non sembrano essere presenti GPU nVidia sul dispositivo"
     device = cuda.Device(0) # Prendo il primo device disponibile
     major, minor = device.compute_capability()
-    attrs = device.get_attributes()
     # Infine settiamo gli attributi rimanenti
-    numberOfThreads     = 1000
-    numberOfProblems    = 2 * numberOfThreads
-    numberOfDenseOutput = numberOfThreads
-    # TODO: Completare
+    numberOfThreads            = 1000
+    numberOfProblems           = 2 * numberOfThreads
+    numberOfDenseOutput        = numberOfThreads
+    threadsPerBlock            = device.get_attributes()[cuda.device_attribute.MAX_THREADS_PER_BLOCK]
+    initialTimeStep            = 1.0e-2
+    preferSharedMemory         = 1
+    maximumTimeStep            = 1.0e+6
+    minimumTimeStep            = 1.0e-14
+    timeStepGrowLimit          = 10.0
+    timeStepShrinkLimit        = 0.2
+    events_directions          = [0 for _ in range(nevents)]
+    denseOutputMinumumTimeStep = 0.0
+    denseOutputSaveFrequency   = 1
+    tolerance                  = [get_tolerance_fromXML(xmlfile) for _ in range(nstates)]
+    return [
+        major, minor,
+        numberOfThreads,          numberOfProblems,  numberOfDenseOutput,
+        threadsPerBlock,          initialTimeStep,   preferSharedMemory,
+        maximumTimeStep,          minimumTimeStep,   timeStepGrowLimit,
+        timeStepShrinkLimit,      events_directions, denseOutputMinumumTimeStep,
+        denseOutputSaveFrequency, tolerance
+    ], device
+
+
+def checkGPUexists(ccapability):
+    """ Controlla l'esistenza della GPU con la capability data in input """
+    assert cuda.Device.count() > 0, "Non sembrano essere presenti GPU nVidia sul dispositivo"
+    capabilities = [cuda.Device(count - 1).compute_capability() for count in range(cuda.Device.count())]
+    assert any(list(filter(lambda x: x == ccapability, capabilities))), "La GPU scelta non esiste"
 
 
 def checkconfig(config_dict):
@@ -80,19 +109,79 @@ def checkconfig(config_dict):
                isinstance(builder_config['modeldefinition']['numberOfDenseOutput'], int) and \
                isinstance(builder_config['modeldefinition']['threadsPerBlock'], int) and \
                isinstance(builder_config['modeldefinition']['preferSharedMemory'], int) and \
-               isinstance(builder_config['modeldefinition']['maximumTimeStep'], int) and \
-               isinstance(builder_config['modeldefinition']['minimumTimeStep'], int) and \
-               isinstance(builder_config['modeldefinition']['timeStepGrowLimit'], int) and \
-               isinstance(builder_config['modeldefinition']['timeStepShrinkLimit'], int) and \
-               isinstance(builder_config['modeldefinition']['denseOutputMinimumTimeStep'], int) and \
+               isinstance(builder_config['modeldefinition']['maximumTimeStep'], float) and \
+               isinstance(builder_config['modeldefinition']['minimumTimeStep'], float) and \
+               isinstance(builder_config['modeldefinition']['timeStepGrowLimit'], float) and \
+               isinstance(builder_config['modeldefinition']['timeStepShrinkLimit'], float) and \
+               isinstance(builder_config['modeldefinition']['denseOutputMinimumTimeStep'], float) and \
                isinstance(builder_config['modeldefinition']['denseOutputSaveFrequency'], int), \
                "Se il campo userdefaultoptions è False allora tutti i restanti devono essere settati e interi"
+        # Controlla che la GPU settata esista
+        checkGPUexists((builder_config['gpu']['major'], builder_config['gpu']['minor']))
 
 
 def getnumevents(xmlfile):
     """ Dall'XML prende il numero di eventi """
     root = ET.parse(xmlfile).getroot()
     return int(root.attrib['numberOfEventIndicators'])
+
+
+def getnumstate(xmlfile):
+    """ Dall'XML prende il numero di stati del sistema """ 
+    root = ET.parse(xmlfile).getroot()
+    return int(root.attrib['numberOfContinuousStates'])
+
+
+def gpu_from_capability(ccapability):
+    """ Dalla cuda capability, ottiene la GPU associata """
+    capabilities = [cuda.Device(count - 1).compute_capability() for count in range(cuda.Device.count())]
+    return cuda.Device(capabilities.index(ccapability))
+
+
+def check_multiple_config(msg1, msg2, msg3, builder_config, keystring, dimension, tipo):
+    """ Fa il check di parametri che potrebbero presentare sottoparametri come nel caso di eventDirection """
+    subparam = builder_config[keystring]
+    # Controllo che il parametro matcha con la dimensione dei valori che si vogliono controllare
+    assert subparam is None and dimension == 0 or subparam is not None and dimension != 0, msg1
+    if subparam is not None: # Se il parametro non è None
+        all_param = subparam['all'] # Prendo l'indicatore all
+        # Controlla se all sia None. Se lo è allora tutti gli altri devono essere settati
+        if all_param is None:
+            # Innanzitutto il numero di sottoparametri deve matchare la dimensione
+            assert len(subparam) - 1 == dimension, msg2
+            # E poi controlla che siano tutti di tipo "tipo"
+            for k, v in subparam.items()[1:]:
+                assert isinstance(v, tipo), msg3
+        else:
+            assert isinstance(all_param, tipo), msg3
+
+
+def getGPUattr2str(attrs, gpu_name):
+    """ Ritorna una stringa riguardo la GPU selezionata """
+    max_len = max([len(f"{k} : {v}") for k, v in attrs.items()])
+    string = ""
+    string += f"DEVICE: {gpu_name}\n"
+    string += "    " + "-"*(max_len + 5) + "\n"
+    string += "    " + "|" + " "*(max_len + 3) + "|\n"
+    for k, v in attrs.items():
+        lenght = len(f"{k} : {v}")
+        string += f"    | {k}: " + " "*(max_len - lenght + 2) + f"{v} |\n"
+    string += "    " + "|" + " "*(max_len + 3) + "|\n"
+    string += "    " + "-"*(max_len + 5) + "\n"
+    return string
+
+
+def conf_dict2str(conf_dict, gpu_attrs):
+    """ Ritorna una stringa che descrive la configurazione settata """
+    string = "CONFIGURAZIONE ESTRAPOLATA\n"
+    for k, v in conf_dict.items():
+        if k == "GPU information":
+            string += getGPUattr2str(gpu_attrs, v)
+        elif isinstance(v, list):
+            string += f"{k}:\n" + "    ".join([f"{k}Value{count + 1}: {v[count]}\n" for count in range(len(v))])
+        else: 
+            string += f"{k}: {v}\n"
+    return string
 
 
 def get_modelica2GPU_configuration(config_file):
@@ -116,20 +205,74 @@ def get_modelica2GPU_configuration(config_file):
         m2g_conf, builder_config = config_dict['modelica2gpu'], config_dict['builder']
         genXML = m2g_conf['generateXML']
         xmlfile = createXML(m2g_conf['workingdir'], m2g_conf['modelfilename']) if genXML else m2g_conf['xml']
-        event_num = getnumevents(xmlfile)
-
-        # Controlla che il numero di eventDirection values sia nel numero degli eventi
-        msg = "Il numero di valori eventDirection è minore/maggiore del numero degli eventi rilevati nel modello"
-        assert builder_config['modeldefinition']['eventDirection'] is None and event_num == 0 or \
-               len(builder_config['modeldefinition']['eventDirection']) == event_num, msg
-        # Controlla anche che tutti i valori dati siano interi, se esistono
-        if builder_config['modeldefinition']['eventDirection'] is not None:
-            for k, v in builder_config['modeldefinition']['eventDirection']:
-                assert isinstance(v, int), "Tutti i valori valueEvent<n> devono essere interi"
-
         notifier, filelogger = m2g_conf['notifier'], m2g_conf['filelogger']
         modelname = xmlfile.split(".")[0].split("/")[-1]
-        # TODO: completare con il getdefaultoption
+        event_num, state_num = getnumevents(xmlfile), getnumstate(xmlfile)
+        builder_options = [builder_config['MPGOSsourcedir']]
+        device = None
+        
+        # Prima però dobbiamo controllare i parametri eventDirection e tolerance
+        if not builder_config['usedefaultoptions']:
+            # Check del parametro eventDirection
+            check_multiple_config(
+                "Il parametro eventDirection è None nonostante ci sono eventi, oppure viceversa"
+                "Il numero di parametri eventDirection non matcha con il numero di eventi rilevati",
+                "Tutti i parametri devono essere di tipo int",
+                builder_config, 'eventDirection', event_num, int
+            )
+
+            # Check del parametro tolerance
+            check_multiple_config(
+                "Il parametro tolerance è None nonostante ci sono stati, oppure viceversa"
+                "Il numero di parametri tolerance non matcha con il numero di stati rilevati",
+                "Tutti i parametri devono essere di tipo float",
+                builder_config, 'tolerance', state_num, float
+            )
+
+            builder_options += [
+                builder_config['gpu']['major'], builder_config['gpu']['minor'],
+                builder_config['modeldefinition']['numberOfThreads'], builder_config['modeldefinition']['numberOfProblems'],
+                builder_config['modeldefinition']['numberOfDenseOutput'], builder_config['modeldefinition']['threadsPerBlock'],
+                builder_config['modeldefinition']['initialTimeStep'], builder_config['modeldefinition']['preferSharedMemory'],
+                builder_config['modeldefinition']['maximumTimeStep'], builder_config['modeldefinition']['minimumTimeStep'],
+                builder_config['modeldefinition']['timeStepGrowLimit'], builder_config['modeldefinition']['timeStepShrinkLimit'],
+                list(builder_config['modeldefinition']['eventDirection'].values()), 
+                builder_config['modeldefinition']['denseOutputMinimumTimeStep'], 
+                builder_config['modeldefinition']['denseOutputSaveFrequency'], 
+                list(builder_config['modeldefinition']['tolerance'].values())
+            ]
+            device = gpu_from_capability((builder_options[1], builder_options[2]))
+        else:
+            def_opts = getdefaultoptions(event_num, state_num, xmlfile)
+            builder_options += def_opts[0]
+            device = def_opts[1]
+
+        config_dict = {
+            "xmlfile"                   : xmlfile,
+            "workingdir"                : m2g_conf['workingdir'],
+            "modelname"                 : modelname,
+            "notifier"                  : notifier,
+            "filelogger"                : filelogger,
+            "MPGOSsourcedir"            : builder_options[0],
+            "GPU information"           : device.name(),
+            "numberOfThreads"           : builder_options[3],
+            "numberOfProblems"          : builder_options[4],
+            "numberOfDenseOutput"       : builder_options[5],
+            "threadsPerBlock"           : builder_options[6],
+            "initialTimeStep"           : builder_options[7],
+            "preferSharedMemory"        : builder_options[8],
+            "maximumTimeStep"           : builder_options[9],
+            "minimumTimeStep"           : builder_options[10],
+            "timeStepGrowLimit"         : builder_options[11],
+            "timeStepShrinkLimit"       : builder_options[12],
+            "eventDirection"            : builder_options[13],
+            "denseOutputMinimumTimeStep": builder_options[14],
+            "denseOutputSaveFrequency"  : builder_options[15],
+            "tolerance"                 : builder_options[16] 
+        }
+        
+        print(conf_dict2str(config_dict, device.get_attributes()))
+        
     except AssertionError as ae:
         msg = f"modelica2GPU ha riscontrato il seguente errore. {ae.args[0]}"
         logger.error(msg, msg)
