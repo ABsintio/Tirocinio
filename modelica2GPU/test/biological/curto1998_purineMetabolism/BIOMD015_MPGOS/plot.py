e4e87f4c32cf8717b86fc2a484370d32b28fa259
ABsintio <absintio098@gmail.com>
2020-11-23 22:35:27 +0100

    Plotted BIOMD015

--- modelica2GPU/test/biological/curto1998_purineMetabolism/BIOMD015_MPGOS/plot.py
+++ modelica2GPU/test/biological/curto1998_purineMetabolism/BIOMD015_MPGOS/plot.py
@@ -16,31 +16,27 @@ with open(filename, mode="r", encoding="utf-8") as f:
 
 npvalues = np.array(values)
 times = npvalues[:, 0]
-x0    = npvalues[:, 1]
-x1    = npvalues[:, 2]
-x2    = npvalues[:, 3]
-x3    = npvalues[:, 4]
-x4    = npvalues[:, 5]
-x5    = npvalues[:, 6]
-x6    = npvalues[:, 7]
-x7    = npvalues[:, 8]
-x8    = npvalues[:, 9]
-x9    = npvalues[:, 10]
-x10    = npvalues[:, 11]
-x11    = npvalues[:, 12]
+x4    = npvalues[:, 5] # 1
+x6    = npvalues[:, 7] # 2
+x7    = npvalues[:, 8] # 1
+x16    = npvalues[:, 17] # 3
+x17    = npvalues[:, 18] # 3
 
-plt.plot(times, x0, marker="_", label="A")
-plt.plot(times, x1, marker="_", label="AL")
-plt.plot(times, x2, marker="_", label="ALL")
-plt.plot(times, x3, marker="_", label="B")
-plt.plot(times, x4, marker="_", label="BL")
-plt.plot(times, x5, marker="_", label="BLL")
-plt.plot(times, x6, marker="_", label="D")
-plt.plot(times, x7, marker="_", label="DL")
-plt.plot(times, x8, marker="_", label="DLL")
-plt.plot(times, x9, marker="_", label="I")
-plt.plot(times, x10, marker="_", label="IL")
-plt.plot(times, x11, marker="_", label="ILL")
+plt.plot(times, x4, marker="_", label="PRPP")
+plt.plot(times, x7, marker="_", label="Gua")
+plt.ylabel("concentrazioni")
+plt.xlabel("Time [s]")
+plt.legend(loc="upper left")
+plt.show()
+
+plt.plot(times, x6, marker="_", label="IMP")
+plt.ylabel("concentrazioni")
+plt.xlabel("Time [s]")
+plt.legend(loc="upper left")
+plt.show()
+
+plt.plot(times, x16, marker="_", label="dATP")
+plt.plot(times, x17, marker="_", label="dGTP")
 plt.ylabel("concentrazioni")
 plt.xlabel("Time [s]")
 plt.legend(loc="upper left")
