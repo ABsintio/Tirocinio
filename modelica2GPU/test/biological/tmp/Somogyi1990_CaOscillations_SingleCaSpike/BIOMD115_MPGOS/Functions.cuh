de7609f81444d4c5ceb7bc92eaaf669005d18a02
ABsintio <absintio098@gmail.com>
2020-12-26 18:49:44 +0100

    Plotted BIOMD115

---
+++ modelica2GPU/test/biological/tmp/Somogyi1990_CaOscillations_SingleCaSpike/BIOMD115_MPGOS/Functions.cuh
@@ -0,0 +1,15 @@
+
+#ifndef BIOMD115_FUNCTIONS_H
+#define BIOMD115_FUNCTIONS_H
+
+#include <iostream>
+
+#define PRECISION double
+
+__device__ PRECISION pow(PRECISION x,PRECISION power){
+    PRECISION y = 0.0;
+    y=pow(x,power);
+    return y;
+}
+
+#endif
