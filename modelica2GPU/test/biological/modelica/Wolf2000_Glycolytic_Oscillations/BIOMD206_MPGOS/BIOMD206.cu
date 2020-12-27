e79e1a3d81e9d61e0d8e29b16ffc09353751d789
ABsintio <absintio098@gmail.com>
2020-12-27 23:04:15 +0100

    Stuff

--- modelica2GPU/test/biological/modelica/Wolf2000_Glycolytic_Oscillations/BIOMD206_MPGOS/BIOMD206.cu
+++ modelica2GPU/test/biological/modelica/Wolf2000_Glycolytic_Oscillations/BIOMD206_MPGOS/BIOMD206.cu
@@ -15,7 +15,7 @@ using namespace std;
 
 #define SOLVER RKCK45 // Runge-Kutta Order 4th
 #define PRECISION double
-const int NT   = 1;
+const int NT   = 10000;
 const int SD   = 9;
 const int NCP  = 1;
 const int NSP  = 19;
@@ -23,7 +23,7 @@ const int NISP = 0;
 const int NE   = 0;
 const int NA   = 0;
 const int NIA  = 0;
-const int NDO  = 100000;
+const int NDO  = 100;
 
 
 void FillSolverObject(
