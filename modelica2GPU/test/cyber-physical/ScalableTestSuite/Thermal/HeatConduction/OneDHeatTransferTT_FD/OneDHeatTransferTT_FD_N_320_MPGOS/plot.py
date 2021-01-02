e7f5779a212de8583c38a191c95b7eeb76935e73
ABsintio <absintio098@gmail.com>
2021-01-02 18:27:28 +0100

    Plotted OneDHeatTransferTT_FD_N_320

--- modelica2GPU/test/cyber-physical/ScalableTestSuite/Thermal/HeatConduction/OneDHeatTransferTT_FD/OneDHeatTransferTT_FD_N_320_MPGOS/plot.py
+++ modelica2GPU/test/cyber-physical/ScalableTestSuite/Thermal/HeatConduction/OneDHeatTransferTT_FD/OneDHeatTransferTT_FD_N_320_MPGOS/plot.py
@@ -16,7 +16,7 @@ with open(filename, mode="r", encoding="utf-8") as f:
 
 npvalues = np.array(values)
 times = npvalues[:, 0]
-vars = [npvalues[:, i] for i in range(1, 159)]
+vars = [npvalues[:, i] for i in range(1, 319)]
 
 plot_number = 1
 idx = 0
