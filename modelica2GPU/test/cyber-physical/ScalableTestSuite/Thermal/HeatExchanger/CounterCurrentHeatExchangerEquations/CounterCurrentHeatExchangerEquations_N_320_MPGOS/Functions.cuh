053a2450f86816e5c3b07ae3bb3390645fffa816
ABsintio <absintio098@gmail.com>
2021-01-04 17:41:36 +0100

    Plotted CounterCurrentHeatExchangerEquations_N_320

---
+++ modelica2GPU/test/cyber-physical/ScalableTestSuite/Thermal/HeatExchanger/CounterCurrentHeatExchangerEquations/CounterCurrentHeatExchangerEquations_N_320_MPGOS/Functions.cuh
@@ -0,0 +1,11 @@
+
+#ifndef CounterCurrentHeatExchangerEquations_N_320_FUNCTIONS_H
+#define CounterCurrentHeatExchangerEquations_N_320_FUNCTIONS_H
+
+#include <iostream>
+
+#define PRECISION double
+
+
+
+#endif
