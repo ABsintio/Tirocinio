eb17c38cca222faa107533eeca1ba2575d9e7e42
ABsintio <absintio098@gmail.com>
2021-01-10 17:25:57 +0100

    Tested PowerSystemStepLoad_N_16_M_4

--- modelica2GPU/test/cyber-physical/ScalableTestSuite/Power/ConceptualPowerSystem/PowerSystem/PowerSystemStepLoad_N_16_M_4_MPGOS/PowerSystemStepLoad_N_16_M_4.cu
+++ modelica2GPU/test/cyber-physical/ScalableTestSuite/Power/ConceptualPowerSystem/PowerSystem/PowerSystemStepLoad_N_16_M_4_MPGOS/PowerSystemStepLoad_N_16_M_4.cu
@@ -13,7 +13,7 @@ using namespace std;
 
 #define SOLVER RKCK45 // Runge-Kutta Order 4th
 #define PRECISION double
-const int NT   = 1;
+const int NT   = 10000;
 const int SD   = 193;
 const int NCP  = 1;
 const int NSP  = 311;
@@ -21,7 +21,7 @@ const int NISP = 18;
 const int NE   = 0;
 const int NA   = 866;
 const int NIA  = 0;
-const int NDO  = 20000;
+const int NDO  = 100;
 
 
 void FillSolverObject(
