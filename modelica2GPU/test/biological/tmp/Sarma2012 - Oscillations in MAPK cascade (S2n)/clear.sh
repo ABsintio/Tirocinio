b3930164f3fd7bbfbe041ac8aa2782bfb0c6b4f8
ABsintio <absintio098@gmail.com>
2021-01-21 14:30:54 +0100

    BIOMD447

--- modelica2GPU/test/biological/tmp/Sarma2012 - Oscillations in MAPK cascade (S2n)/clear.sh
+++ modelica2GPU/test/biological/tmp/Sarma2012 - Oscillations in MAPK cascade (S2n)/clear.sh
@@ -1,3 +1,1 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:050563468b96cecac3eeb89ef0dbf1faa32b60a308d31f47636b5e2ce2460b4a
-size 65
+rm *.o *.c *.h *.json BIOMD444 *.makefile *.log *.libs *_init.xml
