47a4e5c4530986ce8fe19566748f157a248d58ff
ABsintio <absintio098@gmail.com>
2021-01-23 00:10:23 +0100

    Stuff

--- modelica2GPU/test/biological/tmp/scan_dir.py
+++ modelica2GPU/test/biological/tmp/scan_dir.py
@@ -1,3 +1,15 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:57e8eff30ff9912dd7ca2394dc018109b61da5eb5d88f09c255bda9fa559bc89
-size 358
+import os
+
+def scan_dir(current_dir):
+	for file in os.listdir(current_dir):
+		new_path = os.path.join(current_dir, file)
+		if os.path.isdir(new_path):
+			if new_path.endswith("_MPGOS"):
+				print("/".join(new_path.split("/")[:-1]))
+				print(new_path)
+				os.chdir(new_path)
+				os.system("rm *.txt")
+			else:
+				scan_dir(new_path)
+
+scan_dir(os.getcwd())
